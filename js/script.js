30e0960fe03f5eacb4eb948b6e8e9f7acc9a97b2
Gagan Suneja <gagan91suneja@gmail.com>
2017-07-15 15:02:23 +0530

    Major action applied

--- js/script.js
+++ js/script.js
@@ -1,33 +1,134 @@
 $(document).ready(function(){
+
+
 	$('#education_click').click(function(){
 	
 	
 	//alert($('.left_anchor').css('visibility') + $('.education_anchor').css('visibility') );
-
+		
 		if($('.left_anchor').css('visibility')=='hidden')	
 			{
-				$('.left_anchor').  animate({'visibility':'visible'});
+				$('.left_anchor').css('visibility','visible')
 			}
 		else
 			{
-				$('.left_anchor').animate({'visibility':'hidden'});
+				$('.left_anchor').css('visibility','hidden');
 			}
 		
-		if($('.education_anchor').animate('visibility')=='hidden')	
+		
+		if($('.work_anchor').css('visibility')=='visible')
 			{
-				$('.education_anchor').animate({'visibility':'visible'});
+				$('.work_anchor').css('visibility','hidden');
+			}
+		
+
+		if($('.education_anchor').css('visibility')=='hidden')
+			{
+				$('.education_anchor').css('visibility','visible');
 			}
 		else
 			{
 				
-				$('.education_anchor').animate({'visibility':'hidden'});
+				$('.education_anchor').css('visibility','hidden');
 			}
+
+		
 
 		});
 
-	$('.home_button').click(function(){
-		$('.left_anchor').css('visibility','visible');
+	    
+		$('#work_click').click(function(){
+
+			if($('.left_anchor').css('visibility')=='hidden')	
+			{
+					$('.left_anchor').css('visibility','visible')
+			}
+			else
+			{
+					$('.left_anchor').css('visibility','hidden');
+			}
+		
+			if($('.education_anchor').css('visibility')=='visible')
+			{
+				$('.education_anchor').css('visibility','hidden');
+			}
+
+			if($('.work_anchor').css('visibility')=='hidden')
+			{
+				$('.work_anchor').css('visibility','visible');
+			}
+			else
+			{
+				
+				$('.work_anchor').css('visibility','hidden');
+			}
+		});
+
+	    $('#about_me_click').click(function(){
+
+			if($('.right_anchor').css('visibility')=='hidden')	
+			{
+					$('.right_anchor').css('visibility','visible')
+			}
+			else
+			{
+					$('.right_anchor').css('visibility','hidden');
+			}
+		
+			if($('.skills_anchor').css('visibility')=='visible')
+			{
+				$('.skills_anchor').css('visibility','hidden');
+			}
+
+			if($('.about_me_anchor').css('visibility')=='hidden')
+			{
+				$('.about_me_anchor').css('visibility','visible');
+			}
+			else
+			{
+				
+				$('.about_me_anchor').css('visibility','hidden');
+			}
+		});
+
+
+	  $('#skills_click').click(function(){
+
+			if($('.right_anchor').css('visibility')=='hidden')	
+			{
+					$('.right_anchor').css('visibility','visible')
+			}
+			else
+			{
+					$('.right_anchor').css('visibility','hidden');
+			}
+		
+			if($('.about_me_anchor').css('visibility')=='visible')
+			{
+				$('.about_me_anchor').css('visibility','hidden');
+			}
+
+			if($('.skills_anchor').css('visibility')=='hidden')
+			{
+				$('.skills_anchor').css('visibility','visible');
+			}
+			else
+			{
+				
+				$('.skills_anchor').css('visibility','hidden');
+			}
+		});
+		
+
+	$("#home_button_click").click(function(){
+	
 		$('.education_anchor').css('visibility','hidden');
+		$('.work_anchor').css('visibility','hidden');
+		$('.left_anchor').css('visibility','visible');
+		$('.skills_anchor').css('visibility','hidden');
+		$('.about_me_anchor').css('visibility','hidden');
+		$('.right_anchor').css('visibility','visible');
+	
 	});
 
 });
